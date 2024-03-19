fd848665346a2195202c27ea6ed90943cb580612
HazelDaniel <hazeldaniel856@gmail.com>
2024-03-19 03:07:52 +0100

    task-4: submission

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A module for creating and running multiple asyncio Tasks concurrently.
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n, max_delay):
+    """this async function waits for a specified amount of time interval
+        bounded above by max_delay and calls a function"""
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)

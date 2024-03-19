f8b83ea23cf796b082c9ca3d1d8080e2cb61c7b1
HazelDaniel <hazeldaniel856@gmail.com>
2024-03-19 03:12:19 +0100

    task-4: resubmission

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """this async function waits for a specified amount of time interval
         bounded above by max_delay and calls a function"""
     wait_times = await asyncio.gather(
